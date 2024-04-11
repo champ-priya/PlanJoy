e7808a6e5508d50261bd39ff8fbf65f359a21bb4
champ-priya <priya9427749793@gmail.com>
2024-04-11 23:06:48 +0530

    version 2

---
+++ Frontend/src/components/About.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Events</h4>
+      <h2>ABOUT</h2>
+      <p>
+      "Welcome to our event planning hub, where we transform every occasion into a memorable journey. From enchanting birthdays to elegant weddings, thrilling gaming nights to adventurous camping trips, 
+      we specialize in curating experiences that spark joy and excitement.
+       With seamless booking options for venues, entertainment, and unique themes,
+        we're your partner in creating extraordinary events. Let us ignite your 
+        imagination and bring your visions to life. Join us as we redefine celebration, one event at a time. 
+        Start planning your next adventure with us today!"
+      </p>
+    </section>
+  );
+};
+
+export default About;
